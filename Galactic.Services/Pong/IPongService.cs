a3db37a3179c0e3489d986a0dc0d7061fd50f68d
Kiran Jagathlal <kiranjagz@gmail.com>
2021-05-29 21:52:39 +0200

    Added service component.

---
+++ Galactic.Services/Pong/IPongService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galactic.Services.Pong
+{
+    public interface IPongService
+    {
+        Task<string> Ping();
+    }
+}
---
+++ Galactic.Services/Pong/PingService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galactic.Services.Pong
+{
+    public class PingService : IPongService
+    {
+        public Task<string> Ping()
+        {
+            return Task.Run(() =>
+            {
+                return "Pong!";
+            });
+        }
+    }
+}
